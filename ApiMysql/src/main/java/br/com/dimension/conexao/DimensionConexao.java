cd9089ee974a1b40e6a0d0f27fd30096561ca515
Shoyo <shoyo@hotmail.com>
2021-06-06 19:58:37 -0300

    fix

--- ApiMysql/src/main/java/br/com/dimension/conexao/DimensionConexao.java
+++ ApiMysql/src/main/java/br/com/dimension/conexao/DimensionConexao.java
@@ -5,26 +5,27 @@ public class DimensionConexao {
         private static final String url = "jdbc:sqlserver://dimension.database.windows.net:1433;database=Dimension;user=dimension@dimension;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         private static final String username = "dimension";
         private static final String password = "#Gfgrupo10";
+        
         public static Connection createConnectionToSQL() throws Exception {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Conectado ao Banco de dados Azure");
 
             return connection;
     }
-        private static final String urlmql = "jdbc:mysql://0.0.0.0:3306/dimensionBD?autoReconnect=true&useSSL=false";
-        private static final String usernamemql = "root";
-        private static final String passwordmql = "urubu100";
-        public static Connection createConnectiontoMySQL() throws Exception {
-            //String mysqlUrl = "jdbc:mysql://0.0.0.0:3306/dimensionBD?autoReconnect=true&useSSL=false";
-            Connection mysqlCon = DriverManager.getConnection(urlmql, usernamemql, passwordmql);
-            System.out.println("Conectado ao MysqlBD");
-            
-            return mysqlCon;
-        }
+        private static final String urlmsql = "jdbc:mysql://localhost:3306/dimension?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        private static final String usernamemsql= "user_dimension";
+        private static final String passwordmsql = "dimension";
+        
+        public static Connection createConnectionToMySQL() throws Exception {
+            Connection connection = DriverManager.getConnection(urlmsql, usernamemsql, passwordmsql);
+            System.out.println("Conectado ao Banco de dados");
+
+            return connection;
+    }
 
     public static void main(String[] args) throws Exception {
         Connection con= createConnectionToSQL();
-        Connection mysql = createConnectiontoMySQL();
+        Connection mysql = createConnectionToMySQL();
 
         if (con!=null){
             System.out.println("Conectado com sucesso ao Azure");
