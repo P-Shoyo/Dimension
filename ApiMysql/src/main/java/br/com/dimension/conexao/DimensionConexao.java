d8f3dc792accf529568b4ab20b9912e835e4e7d9
Shoyo <shoyo@hotmail.com>
2021-06-06 11:41:47 -0300

    fix api

---
+++ ApiMysql/src/main/java/br/com/dimension/conexao/DimensionConexao.java
@@ -0,0 +1,39 @@
+package br.com.dimension.conexao;
+import java.sql.*;
+
+public class DimensionConexao {
+        private static final String url = "jdbc:sqlserver://dimension.database.windows.net:1433;database=Dimension;user=dimension@dimension;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        private static final String username = "dimension";
+        private static final String password = "#Gfgrupo10";
+        public static Connection createConnectionToSQL() throws Exception {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Conectado ao Banco de dados Azure");
+
+            return connection;
+    }
+        
+        public static Connection createConnectiontoMySQL() throws Exception {
+            String mysqlUrl = "jdbc:mysql://0.0.0.0:3306/Dimension?autoReconnect=true&useSSL=false";
+            Connection mysqlCon = DriverManager.getConnection(mysqlUrl, "root", "urubu100");
+            System.out.println("Conectado ao MysqlBD");
+            
+            return mysqlCon;
+        }
+
+    public static void main(String[] args) throws Exception {
+        Connection con= createConnectionToSQL();
+        Connection mysql = createConnectiontoMySQL();
+
+        if (con!=null){
+            System.out.println("Conectado com sucesso ao Azure");
+            con.close();
+        }
+        
+        if (mysql!=null){
+            System.out.println("Conectado com sucesso ao MySQL");
+            mysql.close();
+        }
+        
+        
+    }
+}
---
+++ ApiMysql/src/main/java/br/com/dimension/dao/DimensionDAO.java
@@ -0,0 +1,53 @@
+package br.com.dimension.dao;
+
+import br.com.dimension.conexao.DimensionConexao;
+import br.com.dimension.insercao.Insercao;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class DimensionDAO {
+
+    public void inserirBD (Insercao insercao){
+        String sql = "INSERT INTO registro(nomeComponente, data, dadosColetados) VALUES (?, ?, ?) ";
+        Connection conn = null;
+        Connection mysql = null;
+        PreparedStatement pstm = null;
+        PreparedStatement psmysql = null;
+        try{
+            conn = DimensionConexao.createConnectionToSQL();
+            pstm = (PreparedStatement) conn.prepareStatement(sql);
+            pstm.setString(1, insercao.getNomeComponente());
+            pstm.setDate(2, new Date(insercao.getData().getTime()));
+            pstm.setDouble(3, insercao.getDadosColetados());
+            pstm.execute();
+            
+            mysql = DimensionConexao.createConnectionToSQL();
+            psmysql = (PreparedStatement) mysql.prepareStatement(sql);
+            psmysql.setString(1, insercao.getNomeComponente());
+            psmysql.setDate(2, new Date(insercao.getData().getTime()));
+            psmysql.setDouble(3, insercao.getDadosColetados());
+            psmysql.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                if (pstm!=null){
+                    pstm.close();
+                }
+                if (conn!=null){
+                    conn.close();
+                }
+                if (mysql!=null){
+                    mysql.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
